43f9181a31bb98b2898702da97321dc8251671be
Arnaud Limbourg <arnaud@fitle.com>
2024-03-04 10:15:21 +0100

    Fix conflicts

--- post-sync.sh
+++ post-sync.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/sh
 
 ## README
@@ -15,22 +14,3 @@ defaults write com.apple.screencapture location -string "$HOME/Desktop"
 
 echo ""
 echo "====> Syncing done!"
-||||||| (empty tree)
-=======
-#!/bin/sh
-
-## README
-# /!\ Ce script d'installation est conçu pour mon usage. Ne le lancez pas sans vérifier chaque commande ! /!\
-
-echo "====> Restoring preferences"
-# echo -e "[storage]\nengine = icloud" >> ~/.mackup.cfg
-
-# Force restore without prompting
-mackup -f restore
-
-# Save screenshots on the desktop
-defaults write com.apple.screencapture location -string "$HOME/Desktop"
-
-echo ""
-echo "====> Syncing done!"
->>>>>>> 3c27cb9 (Update with latest dump)
--- update.sh
+++ update.sh
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-#!/bin/sh
-
-## README
-# /!\ Ce script de mise à jour est conçu pour mon usage. Ne le lancez pas sans vérifier chaque commande ! /!\
-
-brew update
-brew upgrade
-
-brew outdated --cask --greedy --verbose | grep -v '(latest)' | cut -f1 -d" " | xargs -I % sh -c 'brew uninstall --cask %; brew install --cask %;'
-
-# We run neovim-nightly so we need to force the upgrade
-brew reinstall neovim
-
-# We run wezterm-nightly so we need to force the upgrade
-brew upgrade --cask wez/wezterm/wezterm-nightly --no-quarantine --greedy-latest
-
-mas upgrade
-
-brew cleanup -s
-
-brew doctor
-
-echo ""
-echo "==> Update done!"
-||||||| (empty tree)
-=======
 #!/bin/sh
 
 ## README
@@ -47,16 +20,17 @@ brew cleanup -s
 
 brew doctor
 
+# Silence warning about "App from an unidentified developer"
 xattr -dr com.apple.quarantine /Applications/firefox.app/
 xattr -dr com.apple.quarantine /Applications/Notion.app/
 xattr -dr com.apple.quarantine /Applications/Raycast.app/
 xattr -dr com.apple.quarantine /Applications/Transmit.app/
 xattr -dr com.apple.quarantine /Applications/dbgate.app/
 xattr -dr com.apple.quarantine /Applications/WezTerm.app/
+xattr -dr com.apple.quarantine /Applications/Raycast.app/
 xattr -dr com.apple.quarantine /Applications/1Password.app/
 xattr -dr com.apple.quarantine /Applications/Slack.app/
 xattr -dr com.apple.quarantine /Applications/Zed.app/
 
 echo ""
 echo "==> Update done!"
->>>>>>> 3c27cb9 (Update with latest dump)
